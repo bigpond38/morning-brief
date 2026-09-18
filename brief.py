8ee810a4730e97f0e485733430ff6b74a2aec98d
bigpond38 <chiawharry@gmail.com>
2026-09-18 15:22:12 +1000

    Update brief.py

--- brief.py
+++ brief.py
@@ -1,1 +1,13 @@
-print("Hello Harry")
+from google import genai
+import os
+
+client = genai.Client(
+    api_key=os.environ["GEMINI_API_KEY"]
+)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Give me a 3 sentence summary of today's global financial markets."
+)
+
+print(response.text)
